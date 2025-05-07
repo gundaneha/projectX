ab3fcdcd12c30c610aa795fa175de56ef3549cb5
gundaneha <nehagunda07@gmail.com>
2025-05-08 04:49:08 +0530

    Delete jarvis_ai directory

--- jarvis_ai/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from core import views  # Make sure to import views from the 'core' app
-
-urlpatterns = [
-    path("", views.index, name="index"),  # Home page
-    path("ask/", views.ask, name="ask"),   # JARVIS AI endpoint (added trailing slash for consistency)
-]
